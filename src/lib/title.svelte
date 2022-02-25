521c7cd064766319cf256c6c5b6637ec5f046a2e
Seoyun <newseoyun@gmail.com>
2022-02-25 19:57:42 +0900

    new

---
+++ src/lib/title.svelte
@@ -0,0 +1,11 @@
+<script>
+    export let title = "스벨트 하이"
+</script>
+
+<div class="title">
+    <h1>{title}</h1>
+</div>
+
+<style>
+    /* your styles go here */
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,18 @@
+<script>
+</script>
+
+<div class="index">
+  <h2>Welcome!</h2>
+  <a href="/about">About</a>
+  <a href="/hihi">Hihi</a>
+  <br><br>
+  <a href="/hello">HELLO 폴더유</a>
+</div>
+
+<style>
+  .index {
+    text-align: center;
+    display: block;
+    margin: 20px auto;
+  }
+</style>
